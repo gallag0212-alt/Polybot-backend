b55a5653b7c2c0f53c9edcbde0d993fac1553ca6
gallag0212-alt <gallag0212@gmail.com>
2026-04-20 01:04:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const axios = require('axios');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(require('express').static(require('path').join(__dirname,'public')));
 
 // State bot
 const state = {
